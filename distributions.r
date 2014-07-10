587d4b9f4acc2e8d6e337aeab3cf76ac42a9ad92
Chibisi Chima-Okereke <chibisi@activeanalytics.co.uk>
2014-07-10 09:35:03 +0100

    Update distributions.r
    Tidied up R code made it more straight forward

--- distributions.r
+++ distributions.r
@@ -6,19 +6,21 @@ require(microbenchmark)
 sourceCpp("distributions.cpp")
 
 # Benchmarks
-#----------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
 n = 1E3
 
 # R benchmarks
-rbaseOutput <- summary(microbenchmark(runif(n), rbinom(n, size = 10, prob = .5), rgeom(n, prob = .5), rnbinom(n, size = 10, prob = .5), 
-	rpois(n, lambda = 1), rexp(n, rate = 1), rgamma(n, shape = 1, rate = 1), rweibull(n, shape = 1, scale = 1), 
-	rnorm(n, mean = 0, sd = 1), rlnorm(n, mean = 0, sdlog = 1), rchisq(n, df = 5), rcauchy(n), rf(n, df1 = 100, df2 = 100),
+rbaseOutput <- summary(microbenchmark(runif(n), rbinom(n, size = 10, prob = .5), rgeom(n, prob = .5),
+	rnbinom(n, size = 10, prob = .5), rpois(n, lambda = 1), rexp(n, rate = 1),
+	rgamma(n, shape = 1, rate = 1), rweibull(n, shape = 1, scale = 1), rnorm(n, mean = 0, sd = 1), 
+	rlnorm(n, mean = 0, sdlog = 1), rchisq(n, df = 5), rcauchy(n), rf(n, df1 = 100, df2 = 100),
 	unit = "ms"))
 
 # Rcpp benchmarks
-benchmarks <- summary(microbenchmark(runifCpp(n), rbinomCpp(n, size = 10, prob = .5), rgeomCpp(n, prob = .5), rnbinomCpp(n, size = 10, prob = .5), 
-	rpoisCpp(n, lambda = 1), rexpCpp(n, rate = 1), rgammaCpp(n, shape = 1, rate = 1), rweibullCpp(n, shape = 1, scale = 1), 
-	rnormCpp(n, mean = 0, sd = 1), rlnormCpp(n, mean = 0, sdlog = 1), rchisqCpp(n, df = 5), rcauchyCpp(n), rfCpp(n, df1 = 100, df2 = 100),
+benchmarks <- summary(microbenchmark(runifCpp(n), rbinomCpp(n, size = 10, prob = .5), rgeomCpp(n, prob = .5),
+	rnbinomCpp(n, size = 10, prob = .5), rpoisCpp(n, lambda = 1), rexpCpp(n, rate = 1), 
+	rgammaCpp(n, shape = 1, rate = 1), rweibullCpp(n, shape = 1, scale = 1), rnormCpp(n, mean = 0, sd = 1),
+	rlnormCpp(n, mean = 0, sdlog = 1), rchisqCpp(n, df = 5), rcauchyCpp(n), rfCpp(n, df1 = 100, df2 = 100),
 	unit = "ms"))
 
 # Re-basing Rcpp benchmarks over R
@@ -27,7 +29,7 @@ benchmarks[,1] <- c("unif", "binom", "geom", "nbinom", "pois", "exp", "gamma", "
 names(benchmarks)[1] <- "distr"
 benchmarks <- cbind("call" = "Rcpp", benchmarks)
 
-#----------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
 
 require(ggplot2)
 ggplot(benchmarks, aes(distr, median)) + geom_bar(stat = "identity") + 
